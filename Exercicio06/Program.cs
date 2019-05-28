dd2d79f6c9cbf78f08f4738ea6c023837d308c63
Alberto <albertomaronezio@hotmai.com>
2019-05-28 16:50:57 -0300

    Exercicio06

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2 = 0;
+            //Solicita o primeiro número
+            //depois converte (string/int) e
+            //armazena na variavel n1
+            Console.Write("Digite o 1º Número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2º Número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            //imprimindo os números
+            Console.WriteLine("Você digitou: {0} e {1}", n1, n2);
+        }
+    }
+}
